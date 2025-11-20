47e140004f6613a7e5b6b9b1de9014b9f52ecba1
agluzhin <agluzhin@gmail.com>
2025-11-20 16:22:22 +0300

    Initial commit: base Spring Boot project structure

---
+++ src/main/java/agluzhin/pull_request_system/PullRequestSystemApplication.java
@@ -0,0 +1,13 @@
+package agluzhin.pull_request_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PullRequestSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PullRequestSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/agluzhin/pull_request_system/PullRequestSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package agluzhin.pull_request_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PullRequestSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
